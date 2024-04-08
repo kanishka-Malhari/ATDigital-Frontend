ba74a5b848fc6d4c0364c2f894e6a852e5e790cc
kanishka-Malhari <kanishkamalhari@gmail.com>
2024-04-08 09:40:18 +0530

    implement reorder icon

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,25 +1,39 @@
-import React from 'react'
-import logo from '../assets/AT digital.JPG'
-import '../styles/NavBar.css'
+import React, { useState } from 'react'
+import Logo from '../assets/AT digital.JPG'
 import { Link } from 'react-router-dom'
+import ReorderIcon from '@mui/icons-material/Reorder'
+import '../styles/NavBar.css'
 
 function NavBar() {
+const [openLinks, setOpenLinks] = useState(false);
+
+const toggleNavBar = ()=> {
+  setOpenLinks(!openLinks);
+}
+
   return (
     <div className='navbar'>
-        <div className='leftSide'>
-        <img src={logo}/>
-
-        </div>
-
-        <div className='rightSide'>
-            
+      <div className='leftSide' id={openLinks ? "open" : "close"}>
+        <img src={Logo} />
+        <div className='hiddenLinks'>
             <Link to="/services"> Services </Link>
             <Link to="/about"> About Us </Link>
             <Link to="/contact"> Contact Us </Link>
             <Link to="/careers"> Careers </Link>
-
         </div>
+      </div>
       
+      <div className='rightSide'>
+
+            <Link to="/services"> Services </Link>
+            <Link to="/about"> About Us </Link>
+            <Link to="/contact"> Contact Us </Link>
+            <Link to="/careers"> Careers </Link>
+            <button onClick={toggleNavBar}>
+              <ReorderIcon/>
+            </button>
+        
+        </div>
     </div>
   )
 }
